75e2208f4d75e9984962a56e72d7b7f7d84a70e9
giancoppola <44214851+giancoppola@users.noreply.github.com>
2024-06-30 12:24:15 +0100

    clean up

--- src/preload.ts
+++ src/preload.ts
@@ -1,7 +1,7 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 
-import { Display, contextBridge, ipcRenderer } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 
 // App data from context bridge, similar to Model data in .NET MVC should be null checked
 contextBridge.exposeInMainWorld('APP', {
