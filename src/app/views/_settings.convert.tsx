6a899f31f4e1fafeb588e7e753d2205efefa03c3
Gian <giancoppola@hotmail.co.uk>
2024-07-16 17:25:21 +0100

    bg color picker

--- src/app/views/_settings.convert.tsx
+++ src/app/views/_settings.convert.tsx
@@ -1,6 +1,7 @@
 import { App, UserSettings, SupportedImage, supportedImages } from '../types';
 import { FormGroup, FormControlLabel, Switch, Accordion, AccordionSummary,
-AccordionDetails, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+AccordionDetails, FormControl, InputLabel, Select, MenuItem, TextField, 
+InputAdornment} from '@mui/material';
 import { ExpandMore } from '@mui/icons-material';
 import { SetStateAction, useEffect, useState, Dispatch } from 'react';
 
@@ -28,10 +29,34 @@ const Format = (props: {active: boolean, type: SupportedImage, setType: Dispatch
     )
 }
 
+const BackgroundColor = (props: {active: boolean, preserveBg: boolean}) => {
+    const [bgCol, setBgCol]: [string, Dispatch<string>] = useState("fff");
+    return (
+        <>
+            <TextField className='settings-list__input settings-list__text-field'
+            label="BG Colour" name='bg-colour' inputProps={{maxLength: 6}} InputProps={{startAdornment: <InputAdornment position='start'>#</InputAdornment>}}
+            disabled={!props.active || props.preserveBg ? true : false} value={bgCol} onChange={e => setBgCol(e.target.value)} />
+            { props.active && !props.preserveBg && bgCol.length > 0 ?
+            <div className="colour-preview" style={{ background: `#${bgCol}` }}></div>
+            : null
+            }
+        </>
+    )
+}
+
 export const Convert = () => {
     const [active, setActive]: [boolean, Dispatch<boolean>] = useState<boolean>(false);
     const [preserveBg, setPreserveBg]: [boolean, Dispatch<boolean>] = useState<boolean>(false);
     const [type, setType]: [SupportedImage, Dispatch<SupportedImage>] = useState<SupportedImage>("*/*");
+    const HandleActive = () => {
+        if (active) {
+            setActive(false);
+            setPreserveBg(false);
+        }
+        else {
+            setActive(!active);
+        }
+    }
     return (
         <Accordion>
             <AccordionSummary expandIcon={<ExpandMore />}
@@ -42,7 +67,7 @@ export const Convert = () => {
                     <li className="settings-list__item">
                         <FormGroup>
                             <FormControlLabel labelPlacement='start' control={
-                                <Switch checked={active} onChange={() => setActive(!active)} />
+                                <Switch checked={active} onChange={HandleActive} />
                             } label="Convert images to another format" />
                         </FormGroup>
                     </li>
@@ -55,6 +80,9 @@ export const Convert = () => {
                                 <Switch disabled={!active} checked={preserveBg} onChange={() => setPreserveBg(!preserveBg)} />
                             } label="Preserve transparent backgrounds" />
                         </FormGroup>
+                    </li>
+                    <li className="settings-list__item">
+                        <BackgroundColor active={active} preserveBg={preserveBg}/>
                     </li>
                 </ul>
             </AccordionDetails>
