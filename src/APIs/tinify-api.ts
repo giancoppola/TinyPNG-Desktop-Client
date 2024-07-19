069cdcd297057f706404a6e681c90d6c70e68ece
Gian <giancoppola@hotmail.co.uk>
2024-07-19 17:23:08 +0100

    API error handling fixed

--- src/APIs/tinify-api.ts
+++ src/APIs/tinify-api.ts
@@ -6,7 +6,6 @@ import { ImgCompressSettings } from '../app/types';
 export const TinifyAPI = () => {
     ipcMain.handle("tinifyFiles", async (event: Electron.IpcMainInvokeEvent, settings: ImgCompressSettings) => {
 		let files: string[] = [];
-        let errorMsg: string = "";
         try {
             tinify.key = settings.api_key;
             for (const filePath of settings.file_names) {
@@ -14,38 +13,28 @@ export const TinifyAPI = () => {
                 let name = nameArr[nameArr.length - 1];
                 let fileType = "." + name.split(".")[1];
                 name = name.split(".")[0];
-                    await tinify.fromFile(filePath).toFile(`${settings.output_loc}/${name}${fileType}`, (err) => {
-                        console.log('here');
-                        if (err instanceof tinify.AccountError) {
-                            errorMsg = err.message;
-                            console.log(err.message);
-                            return new Error(err.message);
-                        } else if (err instanceof tinify.ClientError) {
-                            errorMsg = err.message;
-                            console.log(err.message);
-                            return new Error(err.message);
-                        } else if (err instanceof tinify.ServerError) {
-                            errorMsg = err.message;
-                            console.log(err.message);
-                            return new Error(err.message);
-                        } else if (err instanceof tinify.ConnectionError) {
-                            errorMsg = err.message;
-                            console.log(err.message);
-                            return new Error(err.message);
-                        } else {
-                            errorMsg = err.message;
-                            console.log(err.message);
-                            return new Error("Random error with Tinify");
-                        }
-                    });
-                files.push(name);
+                try {
+                    await tinify.fromFile(filePath).toFile(`${settings.output_loc}/${name}${fileType}`);
+                    files.push(name);
+                } catch (err) {
+                    if (err instanceof tinify.AccountError) {
+                        throw new Error(err.message);
+                    } else if (err instanceof tinify.ClientError) {
+                        throw new Error(err.message);
+                    } else if (err instanceof tinify.ServerError) {
+                        throw new Error(err.message);
+                    } else if (err instanceof tinify.ConnectionError) {
+                        throw new Error(err.message);
+                    } else {
+                        throw new Error("Random error with Tinify");
+                    }
+                }
             }
         }
         catch (e) {
             console.log(e.message);
             return e.message;
         }
-        console.log("returning");
         return `Compressed ${files}`;
 	});
 }
