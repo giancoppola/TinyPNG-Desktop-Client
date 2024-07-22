0c99008c54280eb189da06b7f0db4bf6003ab713
Gian <giancoppola@hotmail.co.uk>
2024-07-22 16:14:44 +0100

    moving state to main

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { App } from './types';
+import { App, ImgCompressSettings } from './types';
 import { SetStateAction, useEffect, useState, Dispatch } from 'react';
 import { ThemeProvider, CssBaseline, createTheme, Button, Typography } from '@mui/material';
+
+import { UserSettings } from './types';
 
 import { Options } from './views/_settings';
 import { ImgCompress } from './views/_file-drop';
@@ -15,7 +17,41 @@ const darkTheme = createTheme({
 });
 
 const MainWrapper = () => {
-
+	const [settings, setSettings]: [UserSettings, Dispatch<UserSettings>] = useState();
+	const [imgSettings, setImgSettings]: [ImgCompressSettings, Dispatch<ImgCompressSettings>] = useState();
+    const [saveResult, setSaveResult]: [string, Dispatch<string>] = useState("");
+    const [userSettings, setUserSettings]: [UserSettings, Dispatch<UserSettings>] = useState();
+    const [apiKey, setApiKey]: [string, Dispatch<string>] = useState<string>("");
+    const [outputLoc, setOutputLoc]: [string, Dispatch<string>] = useState("");
+	useEffect(() => {
+		GetUserSettings();
+	}, [])
+	const GetUserSettings = async () => {
+		await window.APP.API.getUserSettings()
+		.then(res => {
+			let settings: UserSettings = JSON.parse(res);
+			setSettings(settings);
+			setApiKey(settings.api_key);
+			setOutputLoc(settings.output_location);
+		})
+		.catch(err => {console.error(err)});
+	}
+	const SetUserSettings = async () => {
+        let newSettings: UserSettings = {
+            api_key: apiKey,
+            output_location: outputLoc,
+            overwrite_file: true,
+        };
+        setSettings(newSettings);
+        window.APP.API.setUserSettings(newSettings)
+        .then(result => {
+            console.log(result);
+            setSaveResult(result);
+        });
+        setTimeout(() => {
+            setSaveResult("");
+        }, 10000)
+    }
 	return (
 		<ThemeProvider theme={darkTheme}>
       		<CssBaseline />
